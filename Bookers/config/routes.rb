1b355f45698378e92e6eebe70af5da207a4ed661
nikorasunakeizi <derahiro0619@gmail.com>
2020-02-01 21:45:10 +0900

    [Start] 4-8

---
+++ Bookers/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'books#top'
+
+  get 'books' => 'books#index'
+
+  get 'books/:id' => 'books#show', as: 'show_book'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
+end
